6283b80ac0228e6b9b70e751dff281a9996e793e
Jursdotme <rasmus@jurs.me>
2013-11-13 18:23:32 +0100

    Added Variables for icons

--- js/nav.js
+++ js/nav.js
@@ -1,45 +1,49 @@
-var $navToggle = "#nav-toggle";
+(function($){
+  
+  // Icon Setup
+  var $menu_closed_icon = "&#9776;";
+  var $menu_open_icon = "&#9776;";
+  var $dropdown_open_icon = "&darr;";
+  var $dropdown_closed_icon = "&rarr;";
+  
+  var $navToggle = "#nav-toggle";
 
-$($navToggle).addClass("closed")
+  $($navToggle).addClass("closed")
+  
+  $($navToggle).on("click", function() {
 
-$($navToggle).on("click", function() {
-
-	$("nav#mobile").slideToggle(300);
-
-	var $this = $(this); // this is just for performance
-		if(!$this.hasClass('closed'))
-		$('.closed').toggleClass("closed").toggleClass("open");
-		$this.toggleClass("closed").toggleClass("open");
-
-	$($navToggle+".closed").html("^")
-	$($navToggle+".open").html("T")
-});
+    $("nav#mobile").slideToggle(300);
+    
+    var $this = $(this); // this is just for performance
+    if(!$this.hasClass('closed'))
+    $('.closed').toggleClass("closed").toggleClass("open");
+    $this.toggleClass("closed").toggleClass("open");
+    
+    $($navToggle+".closed").html($menu_closed_icon)
+    $($navToggle+".open").html($menu_open_icon)
+  });
 
 //Add Toggle Buttons to dropdown menus
-$("nav#mobile ul li.has-dropdown > a").append('<span class="icon">2</span>');
-
-
+$("nav#mobile ul li.has-dropdown > a").append('<span class="icon">' + $dropdown_closed_icon + '</span>');
 
 //Make the dropdown menus work
 $("a span.icon").on("click", function() {
 	
-	
-
-	$(this)
+  $(this)
 		.parent()
 			.parent()
 				.find(" > ul")
-						.slideToggle();
-return false;
+          .slideToggle();
+    return false;
 
 });
 
 $("a span.icon").click(function() {
     
-    if ($(this).text() == "2")
-       $(this).html("(")
+    if ($(this).text() == $dropdown_open_icon)
+       $(this).html($dropdown_closed_icon)
     else
-       $(this).html("2");
+       $(this).html($dropdown_open_icon);
 
 });
 
@@ -63,6 +67,6 @@ $(function () {
 		return $( this ).contents().wrap("<p></p>");
 	});
 
-	
-
 });
+    
+})(jQuery);
